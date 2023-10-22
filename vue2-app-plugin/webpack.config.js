df0743da9aecc026b91094ea7977982a17ddd784
ItsOnlyBinary <git@itsonlybinary.com>
2023-10-22 14:23:05 +0100

    vue2-app-plugin use production build so 'vue' is not needed

--- vue2-app-plugin/webpack.config.js
+++ vue2-app-plugin/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: './src/plugin.js',
     output: {
         filename: 'app-plugin.js',
